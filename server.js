34c4a7ac1a5330b56b51742713e2f836464363fb
Vu Truong <vutruongqa@gmail.com>
2017-06-23 16:13:13 +0700

    add some config for heroku

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const PORT = process.env.PORT || 3000;
 
 //handle http and https case
 app.use(function(req,res,next){
-    if(req.headers['x-forwarder-proto'] === 'http'){
+    if(req.headers['x-forwarded-proto'] === 'http'){
         next();
     }
     else{
@@ -18,4 +18,4 @@ app.use(express.static('public'));
 
 app.listen(PORT, function(){
   console.log('Express is up on port 3000');
-})
+});
