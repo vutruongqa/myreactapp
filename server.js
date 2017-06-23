1c28291f6469c8d744af5ae1cfa02701f2e7dfee
Vu Truong <vutruongqa@gmail.com>
2017-06-23 16:09:53 +0700

    add some config for heroku

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(function(req,res,next){
         next();
     }
     else{
-        res.redirect('http://' + req.hostname + req.url)
+        res.redirect('http://' + req.hostname + req.url);
     }
 });
 
