a57c0352366688de4b3fcd2bce607e5b749371ea
Vu Truong <vutruongqa@gmail.com>
2017-06-26 15:38:14 +0700

    fix issue search again after error

--- app/components/ErrorModal.jsx
+++ app/components/ErrorModal.jsx
@@ -1,4 +1,7 @@
 var React = require('react');
+var ReactDOM = require('react-dom');
+var ReactDOMServer = require('react-dom/server');
+
 var ErrorModal = React.createClass({
     getDefaultProps:function(){
       return{
@@ -12,14 +15,8 @@ var ErrorModal = React.createClass({
     },
     
     componentDidMount: function(){
-          var modal = new Foundation.Reveal($('#error-modal'));  
-                modal.open();
-    },
-    
-    render: function(){  
         var {title, message} = this.props;
-        
-        return(
+        var modalMarkup = (
             <div id="error-modal" className="reveal tiny text-center" data-reveal="">
                 <h4>
                     {title}
@@ -34,6 +31,22 @@ var ErrorModal = React.createClass({
                 </p>
             </div>
         );
+        //This code to solve the issue after you search an invalid city then search gain
+        var $modal=$(ReactDOMServer.renderToString(modalMarkup));
+        $(ReactDOM.findDOMNode(this)).html($modal);
+        
+        var modal = new Foundation.Reveal($('#error-modal'));  
+        modal.open();
+    },
+    
+    render: function(){  
+       
+        return(
+            <div>
+                
+            </div>
+        );
+        
     }
 });
 
