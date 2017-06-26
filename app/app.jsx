f7d2b1e775946c45de00d4c8d2c58c52de74067c
Vu Truong <vutruongqa@gmail.com>
2017-06-26 14:21:48 +0700

    add scss files

--- app/app.jsx
+++ app/app.jsx
@@ -9,7 +9,7 @@ var Examples = require('Examples');
 //load foundation
 require('style!css!foundation-sites/dist/foundation.min.css');
 $(document).foundation();
-require('style!css!applicationStyles');
+require('style!css!sass!applicationStyles');
 
 ReactDOM.render(
     <Router history={hashHistory}>
