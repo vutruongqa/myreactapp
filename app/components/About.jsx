ce1469d0250f752c7efe972b6cb6daeff195ca32
Vu Truong <vutruongqa@gmail.com>
2017-06-26 11:12:49 +0700

    update about page

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -12,8 +12,17 @@ var React = require('react');
 var About = (props) => {
         return (
             <div>
-                <h1>About component</h1>
-                <p>Welcome the About page</p>
+                <h1 className="text-center">About</h1>
+                <p>This is a weather page build on React. I have built this for complete react application web course</p>
+                <p>Here are some of the tool i use</p>
+                <ul>
+                    <li>
+                        <a href="https://facebook.github.io/react" target="_blank">React</a> This is javascript framework used
+                    </li>
+                    <li>
+                        <a href="https://openweathermap.org" target="_blank">Open Weather Map</a> I used it to search weather data for city name
+                    </li>
+                </ul>
             </div>
             
         );
--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -1,4 +1,5 @@
 var React = require('react');
+var {Link} = require('react-router');
 
 //var Examples = React.createClass({
 //    render: function(){
@@ -10,7 +11,18 @@ var React = require('react');
 
 var Examples = (props) => {
     return (
-            <h1>Examples component!</h1>
+            <div>
+                <h1 className="text-center">Examples component!</h1>
+            <p>Here are few examples to try out:</p>
+            <ol>
+                <li>
+                    <Link to='/?location=Philadenphia'>Philadenphia, PA</Link>                    
+                </li>
+                <li>
+                    <Link to='/?location=Rio'>Rio, Brazil</Link>
+                </li>
+            </ol>
+            </div>
         )
 };
 
